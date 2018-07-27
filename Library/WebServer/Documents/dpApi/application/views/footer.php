177a7247e0d774798830fb31c47d1752e6d5a9d2
mspanesar145 <mspanesar145@gmail.com>
2018-07-27 11:36:31 +0530

    first commit

---
+++ Library/WebServer/Documents/dpApi/application/views/footer.php
@@ -0,0 +1,14 @@
+
+</div><!-- .animated -->
+</div><!-- .content -->
+</div><!-- /#right-panel -->
+
+<!-- Right Panel -->
+<script src="<?php echo base_url(); ?>assets/js/vendor/jquery-2.1.4.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/plugins.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+
+
+</body>
+</html>
---
+++ Library/WebServer/Documents/dpApi/application/views/welcome_message.php
@@ -0,0 +1,63 @@
+<div class="breadcrumbs">
+    <div class="col-sm-4">
+        <div class="page-header float-left">
+            <div class="page-title">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-8">
+        <div class="page-header float-right">
+            <div class="page-title">
+                <ol class="breadcrumb text-right">
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Users</a></li>
+                    <li class="active">Users List</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content mt-3">
+    <div class="animated fadeIn">
+<div class="col-lg-6">
+            <div class="card">
+                <div class="card-header">
+                    <strong class="card-title">Users List</strong>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col">Handle</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td>Larry</td>
+                          <td>the Bird</td>
+                          <td>@twitter</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        
